9db61a25572ebfd1324a3264c967845c2de1d42e
ahmedmehboob811 <160405430+ahmedmehboob811@users.noreply.github.com>
2025-09-28 14:11:00 +0500

    error resolve

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  resolve: {
+    extensions: ['.js', '.jsx'], // Ensure .jsx is supported
+  },
   server: {
     headers: {
       'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https: blob:; connect-src 'self' https:; font-src 'self' https://fonts.gstatic.com"
